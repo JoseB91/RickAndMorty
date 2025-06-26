e28274e316ad120991c9c5c4daec6bb2de22a218
JoseB91 <jose.briones.r@hotmail.com>
2025-06-26 15:36:51 -0500

    Initial Commit

---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by José Briones on 26/6/25.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
